0e641c62c8c4e137d9373effcecd504762ff4764
davileao <davileao@gmail.com>
2022-11-25 14:23:51 -0300

    runing npm build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 import './reset.css';
-import Home from "./pages/Home";
 import DrawerAppBar from "./components/navbar";
 import ScrollButton from "./components/ScrollTop";
 import Welcome from "./components/welcome";
-import Areas from "./components/areas";
 import SocialMedia from "./components/socialmedia";
 import FloatingActionButtons from "./components/fab";
 import Contact from "./components/contact";
